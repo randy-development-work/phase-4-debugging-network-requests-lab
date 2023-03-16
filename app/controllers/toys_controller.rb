463ce4f75ca8132ec51cb71196a253b659bdafa5
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-03-16 23:33:44 +0300

    Completed Assignment.

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,8 @@
 class ToysController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+
   wrap_parameters format: []
 
   def index
@@ -7,13 +11,20 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+
+    
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    
+      toy = Toy.find_by(id: params[:id])
+      toy.update!(toy_params)
+      render json: toy
+    
+
+    
   end
 
   def destroy
@@ -26,6 +37,14 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_not_found_response
+    render json: { error: "Toy not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
